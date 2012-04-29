38a70514e4843f0d5ef16ef2abd7928001fcc73b
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-29 18:52:54 -0400

    Refactored variable names for improved readability.

--- RedditRIL.pm
+++ RedditRIL.pm
@@ -23,14 +23,14 @@ sub process_subreddit {
     # Process a subreddit,
     # Get a subreddit name & keywords
     # as argument
-    my ($self, $sub, $kws) = @_; #unpack variables from function caller;
+    my ($self, $sub, $keywords) = @_; #unpack variables from function caller;
     my $parser = XML::RSS::Parser::Lite->new(); #Create new parser;
     $parser->parse(get("http://www.reddit.com/r/$sub/new/.rss?limit=5&t=all&sort=new")) or die ("Erf.. a error occured :\n\t$!");
-    my $re  = join "|", @{$kws}; #Stitch together easy list of keywords;
+    my $search_terms  = join "|", @{$keywords}; #Stitch together easy list of keywords;
     my $nothing = 1; # just for cosmetic purpose ;)
     for (my $i = 0; $i < $parser->count(); $i++) {
         my $item = $parser->get($i);
-        if ($item->{title} =~ $re) {
+        if ($item->{title} =~ $search_terms) {
             $self->add_to_ril($item);
             $nothing = 0;
         }
