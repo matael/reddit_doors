0bd3e922106bf8f7aa54e93942616a932ba83316
Conor Schaefer <conor.schaefer@gmail.com>
2012-04-29 18:38:24 -0400

    Renamed several variables for improved readability.

--- RedditRIL.pm
+++ RedditRIL.pm
@@ -19,7 +19,7 @@ sub new {
     return $self;
 }
 
-sub process {
+sub process_subreddit {
     # Process a subreddit,
     # Get a subreddit name & keywords
     # as argument
--- crawler.pl
+++ crawler.pl
@@ -15,8 +15,8 @@ my $config = Load($raw_config);
 
 my $credentials = $config->{credentials};
 my $api = RedditRIL->new(@{$credentials}[0], @{$credentials}[1]);
-foreach my $key (keys %{$config}) {
-	next if $key eq "credentials";
-    say "--> Now processing /r/$key";
-	$api->process($key, $conf->{$key});
+foreach my $subreddit (keys %{$config}) {
+	next if $subreddit eq "credentials";
+    say "--> Now processing /r/$subreddit";
+	$api->process_subreddit($subreddit, $conf->{$subreddit});
 }
